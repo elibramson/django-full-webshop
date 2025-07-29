72303c21ed823b6ad892357f403eaa806fe0efb5
Eli Bramson <eli.bramson@gmail.com>
2025-07-29 22:07:11 +0200

    feat(models): add Category and Product models for e-commerce functionality

--- onlineshop/main/models.py
+++ onlineshop/main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    slug = models.SlugField(max_length=20, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        indexes = [models.Index(fields=['name'])]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    discount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
