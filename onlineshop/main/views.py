fae221b7286266545a108c4206fc9bd869b038fd
Eli Bramson <eli.bramson@gmail.com>
2025-08-11 18:01:15 +0200

    feat(views): add pagination to product list and category URL method

--- onlineshop/main/views.py
+++ onlineshop/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Category, Product
 
 def popular_list(request):
@@ -11,12 +12,16 @@ def product_detail(request, slug):
     return render(request, 'main/product/detail.html', {'product': product})
 
 def product_list(request, category_slug=None):
+    page = request.GET.get('page', 1)
     category = None
     categories = Category.objects.all()
+    paginator = Paginator(products, 1)
+    current_page = paginator.page(int(page))
     products = Product.objects.filter(available=True)
 
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
+        paginator = Paginator(products.filter(category=category), 1)
+        current_page = paginator.page(int(page))
 
-    return render(request, 'main/product/list.html', {'category': category, 'categories': categories, 'products': products})
+    return render(request, 'main/product/list.html', {'category': category, 'categories': categories, 'products': current_page, 'slug': category_slug})
